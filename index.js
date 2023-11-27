29b841cb819bde8cc0c23985ff930da9b8303432
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2023-11-28 00:38:11 +0600

    Loaded Services

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config();
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -12,8 +12,7 @@ app.use(express.json());
 // MongoDB Starts Here
 console.log(process.env.DB_USER);
 
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7grn8zj.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7grn8zj.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,6 +27,37 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // Find Operation
+    const serviceCollection = client.db("eLibrary").collection("services");
+
+    // Loading Services
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // Loading one data API CREATION
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const options = {
+        projection: {
+          title: 1,
+          date: 1,
+          description: 1,
+          image_url: 1,
+          price: 1,
+          time: 1,
+        },
+      };
+
+      const result = await serviceCollection.findOne(query,options);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -35,7 +65,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
